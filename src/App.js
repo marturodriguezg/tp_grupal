acbbbe3463662d6b0e272a3c8b6c8a8871ca3e91
mgonzalez24 <mategonz122@gmail.com>
2022-10-18 11:51:44 -0300

    componentes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,19 @@
-import { VarConstLet } from "./components/fundamentos/01-VarConstLet";
-import { TemplateString } from "./components/fundamentos/02-TemplateString";
+
 
 import './App.css'
-import { JsFunction } from "./components/fundamentos/05-JsFunction";
-import { ObjetosLiterales } from "./components/fundamentos/03-Object";
-import { JsArray } from "./components/fundamentos/04-Array";
 
-import { CardGrid } from "./components/CardGrid/CardGrid";
-import { productos } from "./data/productos";
-import { DesestObj } from "./components/fundamentos/06-DesestObj";
-import { MiPrimerComp } from "./components/MiPrimerComp/MiPrimerComp";
-import { DesestArray } from "./components/fundamentos/07-DesestArray";
+import {Header} from './components/Header/Header'
+/*import { CardGrid } from "./components/CardGrid/CardGrid";
+import { productos } from "./data/productos";*/
+
+
 
 
 function App() {
   //  console.log(dataProducts)
   return (
     <>
-      <MiPrimerComp />
-
-      <VarConstLet />
-
-      <TemplateString />
-
-      <ObjetosLiterales />
-
-      <JsArray /> 
-
-      {/* Aplicación de .map */}
-      {/* <CardGrid data={productos}  /> */}
-
-      <JsFunction />
-
-      {/* <DesestObj /> */}
-      
-      {/* <DesestArray /> */}
-      
+      <Header />
     </>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Header.css'
+
+export const Header = () => {
+    return (
+       <header>
+            <div class="overlay"></div>
+
+            <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop" src="/assets/vid/The Chuck Taylor All Star Converse Made by you.mp4"></video>
+    
+            <div class="container h-100">
+              <div class="d-flex h-100 text-center align-items-center">
+                <div class="w-100 text-white">
+                  <h1 class="display-3">LORIAN</h1>
+                </div>
+              </div>
+            </div>
+        </header>
+    )
+}
